7b6bdfdcb3d778f209e9382ccc5e0fa31be383c5
M-Anwar921 <muhammadzoraiz921@gmail.com>
2026-07-27 23:49:31 +0500

    changes in q1

---
+++ Ch5/CountiesCapitial.py
@@ -0,0 +1,15 @@
+countries = {'Pakistan':'Islamabad', 
+             'China':'Beijing', 
+             'Japan':'Tokoy', ''
+             'Germany': 'Berlin', 
+             'france':"paris"}
+
+print(countries)
+
+print(countries.keys())
+
+print(countries.values())
+
+print('Pakistan' in countries)
+
+print(countries.get('Pakistan'))
